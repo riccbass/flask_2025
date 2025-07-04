5065d1d0cbed2324c384a1ab883d9eec07a29c81
Ricardo <ricardobassanesi@gmail.com>
2025-07-03 21:24:24 -0300

    aula 5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # https://www.youtube.com/watch?v=Lw-zLopB3o0&list=PL-2EBeDYMIbQghmnb865lpdmYyWU3I5F1
 
 from fastapi import FastAPI, HTTPException
-from schemas import GenreURLChoices, Band
+from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
 
 app = FastAPI()
 
@@ -55,13 +55,13 @@ BANDS = [
 # se tem o query parameter, precisa do default caso contrário vai dar erro
 async def bands(genre: GenreURLChoices | None = None,
                 has_albums: bool = False
-                ) -> list[Band]:
+                ) -> list[BandWithID]:
 
-    band_list = [Band(**b) for b in BANDS]
+    band_list = [BandWithID(**b) for b in BANDS]
 
     if genre:
         band_list = [
-            b for b in band_list if b.genre.lower() == genre.value
+            b for b in band_list if b.genre.value.lower() == genre.value
         ]
 
     if has_albums:
@@ -72,8 +72,8 @@ async def bands(genre: GenreURLChoices | None = None,
 
 
 @app.get('/bands/{band_id}', status_code=206)
-async def band(band_id: int) -> Band:
-    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int) -> BandWithID:
+    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
 
     if band is None:
         raise HTTPException(status_code=404, detail='Band not fund')
@@ -85,3 +85,13 @@ async def band(band_id: int) -> Band:
 # o tipo tem que ser correto
 async def about() -> str:
     return 'A great company'
+
+
+@app.post("/bands")
+async def create_band(band_data: BandCreate) -> BandWithID:
+
+    id = BANDS[-1]['id'] + 1
+    band = BandWithID(id=id, **band_data.model_dump())
+    BANDS.append(band.model_dump())
+
+    return band
--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from datetime import date
 
 
@@ -8,20 +8,33 @@ class GenreURLChoices(Enum):
     METAL = 'metal'
     POP_ROCK = 'pop rock'
 
+
+class GenreChoices(Enum):
+    ROCK = 'Rock'
+    METAL = 'Metal'
+    POP_ROCK = 'Pop Rock'
+
+
 # se album não ficar em cima, vai apitar embaixo
 
 
 class Album(BaseModel):
-
     title: str
     release_date: date
 
 
-class Band(BaseModel):
-
-    # {'id':1, 'name': 'Iron Maiden', 'genre': 'Rock',},
-    id: int
+class BandBase(BaseModel):
     name: str
-    genre: str
+    genre: GenreChoices
     # valor default, se não tiver na saída mas não tiver default, dá erro
     albums: list[Album] = []
+
+
+class BandCreate(BandBase):
+    @field_validator('genre', mode='before')
+    def title_case_genre(cls, value):
+        return value.title()  # rock - Rock
+
+
+class BandWithID(BandBase):
+    id: int
